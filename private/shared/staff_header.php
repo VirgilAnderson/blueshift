344992ac1463cd5e9161c51fb26e395d9a7ec4f7
Virgil Anderson <virgil.anderson@gmail.com>
2018-07-19 22:47:53 -0600

    Linked staff_styles.css page to header

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -2,6 +2,9 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Blue Shift tech</title>
+    <title>Blue Shift tech: Staff Page</title>
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff_styles.css'); ?>" />
   </head>
-  
+  <body>
+    <h1>First Header Tag</h1>
+  </body>
