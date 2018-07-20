b83bc7583fdb7a3f251d7697e26e1d25afddb05c
Virgil Anderson <virgil.anderson@gmail.com>
2018-07-19 22:26:50 -0600

    Linked public_footer.php to home page

--- public/index.php
+++ public/index.php
@@ -1,2 +1,3 @@
 <?php require_once('../private/initialize.php'); ?>
-<?php include(SHARED_PATH . '/public_header.php'); ?> 
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+<?php include(SHARED_PATH. '/public_footer.php'); ?>
