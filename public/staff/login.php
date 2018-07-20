84794584d770762b6be408cb18a0266ca26363da
Virgil Anderson <virgil.anderson@gmail.com>
2018-07-20 00:00:52 -0600

    Updated login.php form

--- public/staff/login.php
+++ public/staff/login.php
@@ -3,7 +3,7 @@
 
 <div class="container mt-4">
 
-<h2>Help Another Pet</h2>
+<h2>Login</h2>
 
 <form>
 
@@ -11,21 +11,16 @@
       <legend>Your Info</legend>
 
       <div class="form-group">
-        <label class="form-control-label" for="name">Name</label>
-        <input class="form-control" type="text" id="name" placeholder="Your Name">
+        <label class="form-control-label" for="name">Username</label>
+        <input class="form-control" type="text" id="name" placeholder="Username">
       </div><!-- form-group -->
 
       <div class="form-group">
-        <label class="form-control-label" for="owneremail">Email</label>
-        <input class="form-control" type="text" id="owneremail" placeholder="Address">
+        <label class="form-control-label" >Password</label>
+        <input type="text" class="form-control" placeholder="Password">
       </div><!-- form-group -->
 
-      <div class="form-group">
-        <label class="form-control-label" for="donationamt">Donation Amount</label>
-        <input type="text" class="form-control" id="donationamt" placeholder="Amount">
-      </div><!-- form-group -->
-
-      <button class="btn btn-primary" type="submit">Submit</button>
+      <button class="btn btn-secondary" type="submit">Login</button>
 
     </fieldset><!-- fieldset -->
   </form>
