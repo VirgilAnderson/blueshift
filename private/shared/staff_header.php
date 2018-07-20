e63d4b53971584b36b6ceb88d1e14e5c2da8d861
Virgil Anderson <virgil.anderson@gmail.com>
2018-07-19 23:45:40 -0600

    Changed color of nav

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -10,10 +10,10 @@
     <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff_styles.css'); ?>" />
   </head>
   <body>
-    <nav class="navbar navbar-dark bg-dark navbar-expand-sm">
+    <nav class="navbar navbar-dark  navbar-expand-sm">
       <div class="container">
 
-        <a class="navbar-brand text-uppercase  d-sm-inline-block" href="#"><img class=" mr-2" src="<?php echo url_for('/images/logo.png'); ?>" style="width: 40px" alt="Blue Shift Staff Page">BlueShift Tech</a>
+        <a class="navbar-brand text-uppercase  d-sm-inline-block" href="#"><img class="d-none d-lg-inline mr-2" src="<?php echo url_for('/images/logo.png'); ?>" style="width: 40px" alt="Blue Shift Staff Page">BlueShift Tech</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myToggleNav" aria-controls="myToggleNav" aria-expanded="false" aria-label="Toggle Navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
