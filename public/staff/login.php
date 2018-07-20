62048f53a08f8b43dbe5a3fb3b7d3c065173379d
Virgil Anderson <virgil.anderson@gmail.com>
2018-07-20 01:15:01 -0600

    added breadcrumbs to login.php

--- public/staff/login.php
+++ public/staff/login.php
@@ -3,6 +3,11 @@
 
 <div class="container mt-4">
 
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="<?php echo url_for('index.php'); ?>" >Home</a></li>
+    <li class="breadcrumb-item active">Login</li>
+  </ol>
+
 <form class="col-sm-6">
     <h2>User Login</h2>
     <fieldset class="form-group">
