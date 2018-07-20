13f880e98d717da6a32fc7ec850f970d88502965
Virgil Anderson <virgil.anderson@gmail.com>
2018-07-19 22:44:59 -0600

    Added staff directory and index.php

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -3,4 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Blue Shift tech</title>
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/public_styles.css'); ?>" />
   </head>
+  <body>
+    <h1>First Header Tag</h1>
+  </body>
---
+++ public/staff/index.php
@@ -0,0 +1,3 @@
+<?php require_once('../private/initialize.php'); ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+<?php include(SHARED_PATH. '/staff_footer.php'); ?>
