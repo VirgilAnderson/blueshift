9033d36e75779d91e3c36398479602751e3a812e
Virgil Anderson <virgil.anderson@gmail.com>
2018-07-20 22:46:02 -0600

    Added breadcrumbs to leads, tasks, projects and analytics pages

--- public/staff/analytics/index.php
+++ public/staff/analytics/index.php
@@ -1,2 +1,9 @@
 <?php require_once('../../../private/initialize.php'); ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div class="container" style="margin-top:90px">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="<?php echo url_for('/staff/index.php'); ?>">Dashboard</a></li>
+    <li class="breadcrumb-item active">Analytics</li>
+  </ol>
+</div><!-- .container mt-4 -->
