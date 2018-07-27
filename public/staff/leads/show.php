5fbe51673606b3ea70e3e0cf8d2ea43eeb5b795c
Virgil Anderson <virgil.anderson@gmail.com>
2018-07-26 21:39:15 -0600

    Added a fourth tab to content panes

--- public/staff/leads/show.php
+++ public/staff/leads/show.php
@@ -90,8 +90,9 @@
                     </div><!-- #company_pane -->
                    <div id="history_pane" class="container tab-pane active"><br>
                      <h3>History</h3>
-                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                     <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                    </div><!-- #history -->
+                   
                    <div id="note_pane" class="container tab-pane fade"><br>
                      <h3>Notes</h3>
                      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
