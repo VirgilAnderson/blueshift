d28bc37725fdedfdc507006a047618d25d86bd03
Virgil Anderson <virgil.anderson@gmail.com>
2018-07-26 21:51:18 -0600

    removed active status from history pane on leads/show.php

--- public/staff/leads/show.php
+++ public/staff/leads/show.php
@@ -86,13 +86,16 @@
                   <div class="tab-content">
                     <div id="company_pane" class="container tab-pane active"><br>
                       <h3>Company</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div><!-- #company_pane -->
-                   <div id="history_pane" class="container tab-pane active"><br>
+
+                   <div id="history_pane" class="container tab-pane"><br>
                      <h3>History</h3>
                      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                    </div><!-- #history -->
-                   
+
                    <div id="note_pane" class="container tab-pane fade"><br>
                      <h3>Notes</h3>
                      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
