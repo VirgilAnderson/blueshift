6ed0cd4f645d8590526b025f831c2f8632eb6bf8
Virgil Anderson <virgil.anderson@gmail.com>
2018-07-27 12:52:03 -0600

    Updated the form content on company/new.php

--- public/staff/company/new.php
+++ public/staff/company/new.php
@@ -6,85 +6,80 @@ if(is_post_request()) {
   // Handle form values submitted by new.php
 
   $company = [];
-  $company['first_name'] = isset($_POST['first_name']) ? $_POST['first_name'] : '';
-  $company['last_name'] = isset($_POST['last_name']) ? $_POST['last_name'] : '';
-  $company['phone_direct'] = isset($_POST['phone_direct']) ? $_POST['phone_direct'] : '';
-  $company['email'] = isset($_POST['email']) ? $_POST['email'] : '';
-  $company['role'] = isset($_POST['role']) ? $_POST['role'] : '';
-  $company['lead_source'] = isset($_POST['lead_source']) ? $_POST['lead_source'] : '';
+  $company['company_name'] = isset($_POST['company_name']) ? $_POST['company_name'] : '';
+  $company['company_address'] = isset($_POST['company_address']) ? $_POST['company_address'] : '';
+  $company['company_city'] = isset($_POST['company_city']) ? $_POST['company_city'] : '';
+  $company['company_state'] = isset($_POST['company_state']) ? $_POST['company_state'] : '';
+  $company['company_zip'] = isset($_POST['company_zip']) ? $_POST['company_zip'] : '';
+  $company['company_url'] = isset($_POST['company_url']) ? $_POST['company_url'] : '';
   $company['user_id'] = isset($_POST['user_id']) ? $_POST['user_id'] : '';
 
-  $result = insert_individual($company);
+  $result = insert_company($company);
   if($result === true){
     $new_id = mysqli_insert_id($db);
-    $_SESSION['message'] = 'The lead was created successfully.';
-    redirect_to(url_for('/staff/leads/show.php?id=' . $new_id .'&new=1'));
+    $_SESSION['message'] = 'The company was created successfully.';
+    redirect_to(url_for('/staff/company/show.php?id=' . $new_id .'&new=1'));
   } else {
     $errors = $result;
   }
 } else {
   // Display the blank form
   $company = [];
-  $company['first_name'] = '';
-  $company['last_name'] =  '';
-  $company['phone_direct'] = '';
-  $company['email'] = '';
-  $company['role'] = '';
-  $company['lead_source'] = '';
+  $company['company_name'] = '';
+  $company['company_address'] =  '';
+  $company['company_city'] = '';
+  $company['company_state'] = '';
+  $company['company_zip'] = '';
+  $company['company_url'] = '';
   $company['user_id'] = '';
 }
 
 ?>
 
-<?php $page_title = "New lead"; ?>
+<?php $page_title = "New Company"; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 
 <div class="container" style="margin-top:90px">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="<?php echo url_for('/staff/index.php'); ?>">Dashboard</a></li>
-    <li class="breadcrumb-item"><a href="<?php echo url_for('/staff/leads/index.php'); ?>">Leads</a></li>
-    <li class="breadcrumb-item active">New Lead</li>
+    <li class="breadcrumb-item"><a href="<?php echo url_for('/staff/company/index.php'); ?>">Companies</a></li>
+    <li class="breadcrumb-item active">New Company</li>
   </ol>
-  <form class="col-sm-6" action="<?php echo url_for('/staff/leads/new.php'); ?>"  method="post">
-      <h2>New Lead</h2>
+  <form class="col-sm-6" action="<?php echo url_for('/staff/company/new.php'); ?>"  method="post">
+      <h2>New Company</h2>
       <?php echo display_errors($errors); ?>
       <fieldset class="form-group">
         <legend>Fill in the form to create a new record</legend>
 
         <div class="form-group">
-          <label class="form-control-label" for="first_name">First Name</label>
-          <input class="form-control" type="text" name="first_name" >
+          <label class="form-control-label" for="company_name">Company Name</label>
+          <input class="form-control" type="text" name="company_name" >
         </div><!-- form-group -->
 
         <div class="form-group">
-          <label class="form-control-label" for="last_name">Last Name</label>
-          <input class="form-control" type="text" name="last_name" >
+          <label class="form-control-label" for="company_address">Company Address</label>
+          <input class="form-control" type="text" name="company_address" >
         </div><!-- form-group -->
 
         <div class="form-group">
-          <label class="form-control-label" for="phone_direct">Phone</label>
-          <input class="form-control" type="text" name="phone_direct" >
+          <label class="form-control-label" for="company_city">Company City</label>
+          <input class="form-control" type="text" name="company_city" >
         </div><!-- form-group -->
 
         <div class="form-group">
-          <label class="form-control-label" for="email">Email</label>
-          <input class="form-control" type="text" name="email">
+          <label class="form-control-label" for="company_state">Company State</label>
+          <input class="form-control" type="text" name="company_state">
         </div><!-- form-group -->
 
         <div class="form-group">
-          <label class="form-control-label" for="role">Role</label>
-          <input class="form-control" type="text" name="role">
+          <label class="form-control-label" for="company_zip">Company Zip</label>
+          <input class="form-control" type="text" name="company_zip">
         </div><!-- form-group -->
 
         <div class="form-group">
-          <label for="lead_source">Select list:</label>
-            <select class="form-control" name="lead_source">
-              <option>Web</option>
-              <option>Manual Entry</option>
-              <option>Lead List</option>
-              <option>Call In</option>
-            </select>
+          <label class="form-control-label" for="company_url">Company url</label>
+          <input class="form-control" type="text" name="company_url">
         </div><!-- form-group -->
 
         <div class="form-group">
