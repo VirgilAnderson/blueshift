7040a6fe2b981fb8b6c0fe9f614bae9146e12672
Virgil Anderson <virgil.anderson@gmail.com>
2018-08-13 17:18:36 -0600

    Updated the project index to show company name, employee name and project owner name

--- public/staff/projects/index.php
+++ public/staff/projects/index.php
@@ -34,13 +34,17 @@
               </thead>
               <tbody>
 
-              <?php while($project = mysqli_fetch_assoc($project_set)){ ?>
+              <?php while($project = mysqli_fetch_assoc($project_set)){
+                $company = find_company_by_id($project['company_id']);
+                $individual = find_individual_by_id($project['individual_id']);
+                $admin = find_admin_by_id($project['user_id']);
+                ?>
                 <tr class='clickable-row' data-href="<?php echo url_for('/staff/projects/show.php?id=' . h(u($project['id']))); ?>">
                   <td><?php echo h($project['project_title']); ?></td>
                   <td><?php echo h($project['project_state']); ?></td>
-                  <td><?php echo h($project['company_id']); ?></td>
-                  <td><?php echo h($project['individual_id']); ?></td>
-                  <td><?php echo h($project['user_id']); ?></td>
+                  <td><?php echo h($company['company_name']); ?></td>
+                  <td><?php echo h($individual['first_name']); ?></td>
+                  <td><?php echo h($admin['username']); ?></td>
                 </tr>
               <?php } ?>
             </tbody>
